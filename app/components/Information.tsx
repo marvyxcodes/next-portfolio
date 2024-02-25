8c74386f6107e2ffc8a40d8d03a556928e37c953
marv <trujillomarvin@hotmail.com>
2024-02-25 13:50:12 -0600

    feat: home page work

---
+++ app/components/Information.tsx
@@ -0,0 +1,147 @@
+import React from 'react'
+import budgetyIco from "@/app/images/budgetyIco.png";
+import phoThanhIco from "@/app/images/phoThanhIco.png";
+import weebMaxIco from "@/app/images/weebMax.png";
+import Image from 'next/image';
+import styles from "./Information.module.css";
+
+type Props = {
+}
+
+const Information = (props: Props) => {
+
+
+  let info = (
+    <article id="info" className={styles["article"]}>
+      <p>
+        I have been drawn to computers since I was about 12 years old, I always found myself googling things instead of reading books as it seemed to be a more efficient way of getting the answers to so many questions I had.
+      </p>
+      <p>
+        It wasn't up until sometime in 2021 that I decided I would try my hand at programming and really enjoyed the prospects of it. I consider myself to be a problem solver, so programming came naturally after a brutal effort of learning.
+      </p>
+      <p>
+        Now my main focus is web development, as I have found it is the wild west of technological advancement and does not seem to be slowing down any time soon. With a new web framework popping up every morning, it's a bit hard to get bored of it.
+      </p>
+      <p>
+        Aside from my technological interests, on my days off I enjoy going out to parks and rivers, baking sourdough bread, and spending time with the family.
+      </p>
+    </article>
+  )
+
+  let work = [
+    { name: "Logical Position", title: "Junior Web Developer II", desc: "Implement content deliverables on various client websites using a variety of Content Management Systems(CMS) from Wordpress, Shopify, BigCommerce, to more custom options. Always exercising discretion and using best industry practices as well as QA.", start_date: "2023", end_date: "Present", tech_used: "" },
+
+    { name: "Gates Corp", title: "Distribution Center Associate", desc: "Pick, package, and ship domestically and internationally as part of a team to fulfill customer orders in a timely manner meeting deadlines and quota numbers.", start_date: "2022", end_date: "2022", tech_used: "" },
+
+    { name: "City of Rosemead", title: "Recreation Leader I", desc: "", start_date: "2015", end_date: "2022", tech_used: "" },
+
+    { name: "City of El Monte", title: "Office Assistant", desc: "", start_date: "2019", end_date: "2021", tech_used: "" }
+  ];
+
+  let workElements = work.map((item => {
+    return (
+      <div id="work" key={item.title} className={styles["work-module"]}>
+        <div className={styles["work-dates"]}>
+          <p>{item.start_date + " — " + item.end_date}</p>
+        </div>
+        <div className={styles["work-descriptions"]}>
+          <h3>{item.title}</h3>
+          <h4>{item.name}</h4>
+          <p>{item.desc}</p>
+          <p>{item.tech_used}</p>
+        </div>
+      </div>
+    )
+  }));
+
+  let projects = [
+    {
+      name: "Budgety",
+      picSrc: budgetyIco,
+      imgAlt: "Budget App",
+      tech: ["React", "Bootstrap", "Firestore"],
+      desc: "Cashflow budgeting application with integrated financial news API",
+      href: "https://budgety-5809a.web.app/",
+      repo: "https://github.com/enticingcode/budgety",
+    },
+    {
+      name: "Pho Thanh II",
+      picSrc: phoThanhIco,
+      imgAlt: "Pho Thanh Restaurant",
+      tech: ["React", "Firebase Hosted"],
+      desc: "Landing page for real local restaurant",
+      href: "https://pho-thanh-ii.web.app/",
+      repo: "https://github.com/enticingcode/pho-thanh-page",
+    },
+    {
+      name: "Weeb Max",
+      picSrc: weebMaxIco,
+      imgAlt: "Anime Merchandise Store",
+      tech: ["Next.js", "MongoDB", "Typescript", "Stripe"],
+      desc: "Full Stack E-Commerce Page for all things anime",
+      href: "https://weeb-max.vercel.app",
+      repo: "https://github.com/enticingcode/weebMax",
+    },
+  ];
+
+
+  let projectElements = projects.map((project) => {
+    return (
+      <div id="projects" key={project.name} className="proj-node">
+        <Image
+          // className={styles.projectImage}
+          alt={project.imgAlt}
+          src={project.picSrc}
+          width={300}
+        />
+        <ul className="t-stacks">
+          {project.tech.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+        <div className="proj-info">
+          <h1>{project.name}</h1>
+          <p>{project.desc}</p>
+          <div className="proj-links">
+            <a target="_blank" href={project.href} rel="noreferrer">
+              Demo
+            </a>
+            <a target="_blank" href={project.repo} rel="noreferrer">
+              Repo
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+
+  return (
+    <>
+      <section className={styles["info-section"]}>{info}</section>
+      <section
+        className={`${styles["info-section"]} ${styles["modules-section"]}`}>{workElements}
+      </section>
+      <section
+        className={`${styles["info-section"]} ${styles["modules-section"]}`}>{projectElements}
+      </section>
+    </>
+  )
+}
+
+export default Information
+
+
+
+
+
+
+
+
+
+
+
+{/* <h2 className="section-title">Projects</h2>
+<div className="proj-list d-flex flex-wrap flex-xl-nowrap">
+  {projectElements}
+</div> */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,139 +1,86 @@
-import React from "react";
-import linkedIn from "@/app/images/linked-in.svg";
-import budgetyIco from "@/app/images/budgetyIco.png";
-import phoThanhIco from "@/app/images/phoThanhIco.png";
-import weebMaxIco from "@/app/images/weebMax.png";
-import Image from "next/image";
+"use client";
 
+import React, { useState } from "react";
+import linkedIn from "@/app/images/linked-in.svg";
+import Image from "next/image";
+import Link from "next/link";
+import Information from "./components/Information";
 import styles from "./page.module.css";
 
-const Bio = () => {
-    //   const skills = {
-    //     languages: ["Javascript", "TypeScript"],
-    //     frameworks: ["React.js / Redux", "Next.js", "Express.js"],
-    //     other: [
-    //       "HTML",
-    //       "CSS",
-    //       "EJS",
-    //       "Mongoose",
-    //       "Bootstrap",
-    //       "Node.js",
-    //       "Tailwind UI",
-    //     ],
-    //     databases: ["Firebase", "mySQL", "MongoDB"],
-    //   };
+const Home = () => {
 
-    let projects = [
-        {
-            name: "Budgety",
-            picSrc: budgetyIco,
-            imgAlt: "Budget App",
-            tech: ["React", "Bootstrap", "Firestore"],
-            desc: "Cashflow budgeting application with integrated financial news API",
-            href: "https://budgety-5809a.web.app/",
-            repo: "https://github.com/enticingcode/budgety",
-        },
-        {
-            name: "Pho Thanh II",
-            picSrc: phoThanhIco,
-            imgAlt: "Pho Thanh Restaurant",
-            tech: ["React", "Firebase Hosted"],
-            desc: "Landing page for real local restaurant",
-            href: "https://pho-thanh-ii.web.app/",
-            repo: "https://github.com/enticingcode/pho-thanh-page",
-        },
-        {
-            name: "Weeb Max",
-            picSrc: weebMaxIco,
-            imgAlt: "Anime Merchandise Store",
-            tech: ["Next.js", "MongoDB", "Typescript", "Stripe"],
-            desc: "Full Stack E-Commerce Page for all things anime",
-            href: "https://weeb-max.vercel.app",
-            repo: "https://github.com/enticingcode/weebMax",
-        },
-    ];
+  const [currentNavigation, setCurrentNavigation] = useState("info");
 
-    let projectElements = projects.map((project) => {
-        return (
-            <div key={project.name} className="proj-node">
-                <Image
-                    className={styles.projectImage}
-                    alt={project.imgAlt}
-                    src={project.picSrc}
-                    width={300} 
-                    />
-                <ul className="t-stacks">
-                    {project.tech.map((item) => {
-                        return <li key={item}>{item}</li>;
-                    })}
-                </ul>
-                <div className="proj-info">
-                    <h1>{project.name}</h1>
-                    <p>{project.desc}</p>
-                    <div className="proj-links">
-                        <a target="_blank" href={project.href} rel="noreferrer">
-                            Demo
-                        </a>
-                        <a target="_blank" href={project.repo} rel="noreferrer">
-                            Repo
-                        </a>
-                    </div>
-                </div>
-            </div>
-        );
-    });
+  // function handleNavigation(e) {
+  //   e.preventDefault();
+  //   const target = e.target.id;
+  //   setCurrentNavigation(target);
+    
+  // }
 
-    return (
-        <main className={styles.home}>
-            {/* INTRO BIO SECTION  */}
-            <header id="top" className={styles.biocard}>
-                <div className={styles['bio-div']}>
-                    <h1 className="">Marvin Trujillo</h1>
-                    <h2>Full Stack Developer</h2>
-                    <p>I help develop technological solutions.</p>
-                    {/* <a
+  // console.log(currentNavigation);
+  
+
+  return (
+    <main className={styles.home}>
+      {/* INTRO BIO SECTION  */}
+      <header id="top" className={styles.biocard}>
+        <div className={styles['bio-div']}>
+          <h1 className="">Marvin Trujillo</h1>
+          <h2>Full Stack Developer</h2>
+          <p></p>
+          
+        </div>
+        <nav className={styles["nav"]}>
+          <ul className={styles['nav-list']}>
+            <li><a href="#info">Info</a></li>
+            <li><a href="#work">Work</a></li>
+            <li><a href="#projects">Projects</a></li>
+          </ul>
+        </nav>
+      </header>
+
+
+
+
+      {/* PROJECTS SECTION */}
+      
+    <section className={styles.content}>
+      <Information />
+    </section>
+    </main>
+  );
+};
+
+export default Home;
+
+
+
+{/* <a
                         href="https://www.linkedin.com/in/marvin-trujillo-b18b84132/"
                         target="_blank"
                         rel="noreferrer"
                     >
                         <Image src={linkedIn} alt="LinkedIn Social" />
                     </a> */}
-                </div>
-                <div>
-                    <ul className={styles['nav-list']}>
-                        <li>Info</li>
-                        <li>Work</li>
-                        <li>Projects</li>
-                    </ul>
-                </div>
-            </header>
-
-            {/* PROJECTS SECTION */}
-            <section className={styles.content}>
-                <article className="">
-                    <p>
-                        I am a self taught developer based in Arkansas, USA. I have learned
-                        to develop responsive websites from the ground up along with a
-                        general understanding of software engineering. My discipline and
-                        persistence has led me to build projects for fun but also delve into
-                        a career of Frontend Web Development. I'm excited to continuously
-                        learn new developing technologies as well older tried and tested
-                        ones.
-                    </p>
-                </article>
-                <h2 className="section-title">Projects</h2>
-                <div className="proj-list d-flex flex-wrap flex-xl-nowrap">
-                    {projectElements}
-                </div>
-            </section>
-
-        </main>
-    );
-};
-
-export default Bio;
 
 
+//   const skills = {
+  //     languages: ["Javascript", "TypeScript"],
+  //     frameworks: ["React.js / Redux", "Next.js", "Express.js"],
+  //     other: [
+  //       "HTML",
+  //       "CSS",
+  //       "EJS",
+  //       "Mongoose",
+  //       "Bootstrap",
+  //       "Node.js",
+  //       "Tailwind UI",
+  //     ],
+  //     databases: ["Firebase", "mySQL", "MongoDB"],
+  //   };
+
+  
 // {/* SKILLS SECTION */}
 // <div className="d-flex justify-content-end">
 // <div className="top-link">
